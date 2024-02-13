bed141372ab99d22f0cb84da4ffa8c2f1c569dfa
Berk TURKUCU <95139164+turkucuberk@users.noreply.github.com>
2024-02-13 13:43:21 +0300

    Update Serenity Framework Configurations (#4)
    * update serenity framework
    
    * update serenity framework
    
    * Update Serenity Framework Configurations

--- src/test/java/com/lambdatest/LambdaTestSerenityDriver.java
+++ src/test/java/com/lambdatest/LambdaTestSerenityDriver.java
@@ -3,12 +3,12 @@ package com.lambdatest;
 import java.net.URL;
 import java.util.Iterator;
 
+import net.thucydides.core.environment.SystemEnvironmentVariables;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import net.thucydides.core.util.EnvironmentVariables;
-import net.thucydides.core.util.SystemEnvironmentVariables;
 import net.thucydides.core.webdriver.DriverSource;
 
 public class LambdaTestSerenityDriver implements DriverSource {
@@ -16,17 +16,18 @@ public class LambdaTestSerenityDriver implements DriverSource {
 	public WebDriver newDriver() {
 		EnvironmentVariables environmentVariables = SystemEnvironmentVariables.createEnvironmentVariables();
 
+		String environment = "single";
+
 		String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");         //Add your LT username here
 		String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");    //Add your LT accessKey here
 		String appId = System.getenv("LT_APP_ID") == null ? "lt://proverbial-android" : System.getenv("LT_APP_ID");    //Add your LT appId here
 		String gridUrl = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");    //Add LT gridUrl here
 
-		String environment = System.getProperty("environment");
-		
+
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("plugin","Serenity LambdaTest Plugin");
+		capabilities.setCapability("plugin", "Serenity LambdaTest Plugin");
 		capabilities.setCapability("isRealMobile", true);
-		capabilities.setCapability("app",appId);     //Enter app url here
+		capabilities.setCapability("app", appId);     //Enter app url here
 
 		Iterator it = environmentVariables.getKeys().iterator();
 		while (it.hasNext()) {
@@ -38,15 +39,14 @@ public class LambdaTestSerenityDriver implements DriverSource {
 				capabilities.setCapability(key.replace("lt_", ""), environmentVariables.getProperty(key));
 
 			} else if (environment != null && key.startsWith("environment." + environment)) {
-				
+
 				capabilities.setCapability(key.replace("environment." + environment + ".", ""),
-						environmentVariables.getProperty(key));
+					environmentVariables.getProperty(key));
 			}
 		}
 
 		try {
-			String url = "https://" + username + ":" + accessKey + "@" + gridUrl
-					+ "/wd/hub";
+			String url = "https://" + username + ":" + accessKey + "@" + gridUrl + "/wd/hub";
 			return new RemoteWebDriver(new URL(url), capabilities);
 		} catch (Exception e) {
 			System.out.println(e);
--- src/test/java/com/lambdatest/cucumber/SingleTest.java
+++ src/test/java/com/lambdatest/cucumber/SingleTest.java
@@ -1,11 +1,11 @@
 package com.lambdatest.cucumber;
 
 import com.lambdatest.LambdaTestSerenityTest;
-import cucumber.api.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/single.feature")
+@CucumberOptions(features = "src/test/resources/features", tags = "@single_feature")
 public class SingleTest extends LambdaTestSerenityTest {
 }
